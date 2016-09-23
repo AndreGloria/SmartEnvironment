042fd53f5be57db34df450b3a7dae21170818d85
AndreGloria <ag_andre_gloria@hotmail.com>
2016-09-23 17:07:55 +0100

    Updated USB connection program to function with modes

--- Arduino/USB/USB.ino
+++ Arduino/USB/USB.ino
@@ -1,15 +1,39 @@
+#include "DHT.h"
+
 #define RELAY1 7
+#define DHTPIN 2
+#define DHTTYPE DHT11
 
 String action = "";
 String port = "";
 String value = "";
+String mode = "mod1";
 boolean stringCompleted = false;  // whether the string is complete
 boolean actionPart = true;
 boolean portPart = false;
 boolean valuePart = false;
 
+int led1 = 6;
+int mod1 = 11;
+int mod2 = 10;
+int mod3 = 9;
+
+long lastSendAttempt = 0;
+long lastSendAttempt2 = 0;
+
+bool change = true;
+
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup() {
     pinMode(RELAY1, OUTPUT);
+    pinMode(led1, OUTPUT);
+    pinMode(mod1, OUTPUT);
+    pinMode(mod2, OUTPUT);
+    pinMode(mod3, OUTPUT);
+    digitalWrite(mod1, HIGH);
+    
+    dht.begin();
 
     Serial.begin(9600); // start serial for output
 }
@@ -21,15 +45,47 @@ void loop(){
         digitalWrite(port.toInt(), HIGH);
       if(value == "low")
         digitalWrite(port.toInt(), LOW);
+    } else {
+      if(action == "mode"){
+        if(port == "mod1"){ 
+            digitalWrite(mod1, HIGH);
+            digitalWrite(mod2, LOW);
+            digitalWrite(mod3, LOW);
+            mode = "mod1";
+        } else {
+          if(port == "mod2"){ 
+            digitalWrite(mod2, HIGH);
+            digitalWrite(mod1, LOW);
+            digitalWrite(mod3, LOW);
+            mode = "mod2";
+          } else {
+            if(port == "mod3"){  
+              digitalWrite(mod3, HIGH);
+              digitalWrite(mod2, LOW);
+              digitalWrite(mod1, LOW);
+              mode = "mod3";
+            }
+          }
+        }
+      }
     }
-    Serial.println(action);
-    Serial.println(port);
-    Serial.println(value);
     action = "";
     port = "";
     value = "";
     stringCompleted = false;
   }
+
+    if(mode == "mod1"){ 
+        normal();
+    } else {
+      if(mode == "mod2"){ 
+        maintenance();
+      } else {
+        if(mode == "mod3"){  
+          hibernation();
+        }
+      }
+    }
 }
 
 void serialEvent() {
@@ -60,7 +116,67 @@ void serialEvent() {
           if(valuePart)
             value += inChar;
         }
-      } 
-    } 
+      }
+    }
   }
 }
+
+void normal(){
+  long now = millis();
+  if (now - lastSendAttempt > 10000) {
+    lastSendAttempt = now;
+
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+    Serial.print("sensor:");
+    Serial.print("hum1,");
+    Serial.println(h);
+    Serial.print("sensor:");
+    Serial.print("temp1,");
+    Serial.println(t);
+  }
+  if (now - lastSendAttempt2 > 5000) {
+    lastSendAttempt2 = now;
+    
+    change = !change;
+  }
+  
+  if(change){
+    digitalWrite(RELAY1, HIGH);
+  } else {
+    digitalWrite(RELAY1, LOW);
+  }
+}
+
+void maintenance(){
+  long now = millis();
+  if (now - lastSendAttempt > 10000) {
+    lastSendAttempt = now;
+
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+    Serial.print("sensor:");
+    Serial.print("hum1,");
+    Serial.println(h);
+    Serial.print("sensor:");
+    Serial.print("temp1,");
+    Serial.println(t);
+  }
+  if (now - lastSendAttempt2 > 10000) {
+    lastSendAttempt2 = now;
+
+    change = !change;
+  }
+  
+  if(change){
+    digitalWrite(RELAY1, HIGH);
+  } else {
+    digitalWrite(RELAY1, LOW);
+  }
+  
+}
+
+void hibernation(){
+   digitalWrite(RELAY1, LOW);
+}
+
